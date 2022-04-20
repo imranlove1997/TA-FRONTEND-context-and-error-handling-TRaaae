1531006ed42d24c9af30af1e0dd67d36322ad499
Imran Khan <imranlove1997@gmail.com>
2022-04-20 12:26:07 +0530

    React HOC

---
+++ block-BRaadb/hoc-react/src/index.js
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+ 
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
